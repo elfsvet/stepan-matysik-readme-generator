a8ec0b3c3dd04a420e0542290c282c6b8a91cf49
elfsvet <stepanmatysik@gmail.com>
2022-05-05 22:23:02 -0400

    installed inquirer didn't use it to set prompt

--- index.js
+++ index.js
@@ -33,3 +33,20 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+// var inquirer = require('inquirer');
+// inquirer
+//   .prompt([
+//     /* Pass your questions in here */
+//   ])
+//   .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
