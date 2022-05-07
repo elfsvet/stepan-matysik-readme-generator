0458a77b61c4724816e1eac28470654f011e8ff0
elfsvet <stepanmatysik@gmail.com>
2022-05-07 10:06:12 -0400

    add write file function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 // added function from generateMarkdown to make them available here.
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
@@ -7,10 +8,23 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok:true,
+                message: 'File created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
